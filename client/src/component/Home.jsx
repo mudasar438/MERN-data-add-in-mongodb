2a6b225bd20829b1a14d0abb8f50e3a17422e535
mudasar438 <muhammadmudasar438@gmail.com>
2022-07-01 17:01:33 +0500

    after submit from inputs empty and solve all bugs

--- client/src/component/Home.jsx
+++ client/src/component/Home.jsx
@@ -22,15 +22,21 @@ const Home = () => {
   const submitbtn = ()=>{
     console.log("You are click on add button");
     addUser(user)
+    
+  setUser(inpvalues);
+
+    
   toast.success("User Added Successfully", {
     position: "top-right",
     autoClose: 1000,
-  })
+  }
+  
+  
+  )
 
 
     // alert("Data ADD Into MongoBD")
 
-    
   }
   return (
     <>
@@ -45,19 +51,19 @@ const Home = () => {
       <div className="my-2 w-[80%]  mx-auto">
         <label htmlFor="" className='text-xl py-2'>Name :</label>
         <br />
-        <input type="text" placeholder='username' name='name' onChange={(e)=>handleChange(e)} className='border rounded-md w-full p-2 text-black border-black  '/>
+        <input type="text" placeholder='username' name='name' value={user.name} onChange={(e)=>handleChange(e)} className='border rounded-md w-full p-2 text-black border-black  '/>
       </div>
       <div className="my-2 w-[80%]  mx-auto ">
         <label htmlFor="">Email</label> <br />
-        <input type="text" placeholder='Email@gmail.com' name='email' className="border rounded-md w-full text-black p-2 border-black " onChange={(e)=>handleChange(e)}   />
+        <input type="text" placeholder='Email@gmail.com' name='email' value={user.email} className="border rounded-md w-full text-black p-2 border-black " onChange={(e)=>handleChange(e)}   />
       </div>
       <div className="my-2 w-[80%]  mx-auto ">
         <label htmlFor="">Phone</label>
-        <input type="text" placeholder='+923564296' name='phone' onChange={(e)=>handleChange(e)} className='border text-black rounded-md w-full p-2 border-black '/>
+        <input type="text" placeholder='+923564296' name='phone' value={user.phone} onChange={(e)=>handleChange(e)} className='border text-black rounded-md w-full p-2 border-black '/>
       </div>
       <div className="my-2 w-[80%]  mx-auto ">
         <label htmlFor="">City</label> <br />
-        <input type="text" placeholder='FSD' name='city' onChange={(e)=>handleChange(e)} className='rounded-md  text-black w-full p-2 border border-black '/>
+        <input type="text" placeholder='FSD' name='city' value={user.city} onChange={(e)=>handleChange(e)} className='rounded-md  text-black w-full p-2 border border-black '/>
       </div>
       <div className=" bg-slate-500 rounded-md border p-2 border-gray-700 w-[80%] mx-auto  text-center mr-auto">
        <button onClick={submitbtn} >Submit</button>
--- client/src/component/alluser.js
+++ client/src/component/alluser.js
@@ -1,125 +1,128 @@
-import React from 'react'
-import { useEffect , useState} from 'react'
-import {getUsers} from '../serves/api';
-import {deleteuser} from '../serves/api';
-import { Link } from 'react-router-dom';
-import Search from './search';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
-
+import React from "react";
+import { useEffect, useState } from "react";
+import { getUsers } from "../serves/api";
+import { deleteuser } from "../serves/api";
+import { Link } from "react-router-dom";
+import Search from "./search";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Alluser = () => {
-
   const [users, setUsers] = useState([]);
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
 
   const handleSearch = (e) => {
     setSearch(e.target.value);
-
-  }
+  };
   console.log(search);
-    useEffect(() => {
-      getalluser()
+  useEffect(() => {
+    console.log('clicked')
+    getalluser();
+  }, []);
+console.log(users,"users")
+  const getalluser = async () => {
+    try{
 
-    }, [])
-
-    const getalluser = async() => {
       let response = await getUsers();
-      console.log(response.data);
       setUsers(response.data);
-
     }
+    catch(error){
+      console.error(error)
+    }
+  };
 
-    const SearchFilter = users.filter(user => {
-      return user.name.toLowerCase().includes(search.toLowerCase());
-    })
+  const SearchFilter = 
+  users.filter((user) => {
+    return user.name?.toLowerCase().includes(search.toLowerCase());
+  });
 
-    const deleteUserDetail =async(id)=>{
-      await deleteuser(id);
-      getalluser()
-      toast.success("User Delete Successfully", {
-        position: "top-right",
-        autoClose: 1000,
-
+  const deleteUserDetail = async (id) => {
+    await deleteuser(id)
+      .then((res) => {
+        getalluser();  
+        if (res.status === 200) {
+          toast.success("User Delete Successfully", {
+            position: "top-right",
+            autoClose: 1000,
+          });
+        } else {
+          toast.error("User Delete Failed", {
+            position: "top-right",
+            autoClose: 5000,
+          });
+        }
       })
-
-    }
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
-   <>
-   <div className="max-w-screen-2xl">
-    <Search  handleSearch={handleSearch}/>
-    {SearchFilter.length ? ( 
-       <div className="w-[80%] mx-auto text-white flex my-10 ">
-       <table className="table-auto w-[100%] text-left whitespace-no-wrap border-separate bg-gray-800">
-                   <thead >
-                     <tr>
-                       <th className="px-4 py-3   tracking-wider font-medium text-white bg-gray-900 rounded-tl rounded-bl">
-                         Name
-                       </th>
-                       <th className="px-4 py-3   tracking-wider font-medium text-white bg-gray-900">
-                         Email{" "}
-                       </th>
-                      
-                       
-                       <th className="px-4 py-3   tracking-wider font-medium text-white bg-gray-900">
-                         Phone
-                       </th>
-                       <th className="px-4 py-3   tracking-wider font-medium text-white bg-gray-900">
-                         City
-                       </th>
-                       <th className="px-4 py-3 text-center  tracking-wider font-medium text-white bg-gray-900">
-                         Operation
-                       </th>
-                     </tr>
-                   </thead>
-                   <tbody>
-                     {SearchFilter.map((user, index) =>{
-                      console.log(user);
-                       return(
-                         <tr key={index} className="space-y-3">
-                           <td className="px-4 py-2">{user.name}</td>
-                           <td className="px-4 py-2">{user.email}</td>
-                           <td className="px-4 py-2">{user.phone}</td>
-                           <td className="px-4 py-2">{user.city}</td>
-                           {/* <td className='px-4 py-2'> {user._id}</td> */}
-                           <td className="px-4 py-2 space-x-3 flex justify-center">
-                         <Link to = {`/edit/${user._id}`}>
-                         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded-md">
-                               Edit
-                             </button>
-                         </Link>
-                             <div className="">
+    <>
+      <div className="max-w-screen-2xl">
+        <Search handleSearch={handleSearch} />
+        {SearchFilter.length ? (
+          <div className="w-[80%] mx-auto text-white flex my-10 ">
+            <table className="table-auto w-[100%] text-left whitespace-no-wrap border-separate bg-gray-800">
+              <thead>
+                <tr>
+                  <th className="px-4 py-3   tracking-wider font-medium text-white bg-gray-900 rounded-tl rounded-bl">
+                    Name
+                  </th>
+                  <th className="px-4 py-3   tracking-wider font-medium text-white bg-gray-900">
+                    Email{" "}
+                  </th>
 
-                             <button onClick={()=> deleteUserDetail(user._id)} className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-3 rounded-md">
-                               Delete
-                             </button>
-                             <ToastContainer />
-                             </div>
-                           </td>
-                         </tr>
-                       )
-                     })}
-   
-                     
-                         
-                     
-                   </tbody>
-                 </table>
-       </div>
+                  <th className="px-4 py-3   tracking-wider font-medium text-white bg-gray-900">
+                    Phone
+                  </th>
+                  <th className="px-4 py-3   tracking-wider font-medium text-white bg-gray-900">
+                    City
+                  </th>
+                  <th className="px-4 py-3 text-center  tracking-wider font-medium text-white bg-gray-900">
+                    Operation
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {SearchFilter.map((user, index) => {
+                  // console.log(user);
+                  return (
+                    <tr key={index} className="space-y-3">
+                      <td className="px-4 py-2">{user.name}</td>
+                      <td className="px-4 py-2">{user.email}</td>
+                      <td className="px-4 py-2">{user.phone}</td>
+                      <td className="px-4 py-2">{user.city}</td>
+                      {/* <td className='px-4 py-2'> {user._id}</td> */}
+                      <td className="px-4 py-2 space-x-3 flex justify-center">
+                        <Link to={`/edit/${user._id}`}>
+                          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded-md">
+                            Edit
+                          </button>
+                        </Link>
+                        <div className="">
+                          <button
+                            onClick={() => deleteUserDetail(user._id)}
+                            className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-3 rounded-md"
+                          >
+                            Delete
+                          </button>
+                          <ToastContainer />
+                        </div>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        ) : (
+          <div className="text-2xl text-center text-white mt-12 lg:text-4xl font-bold">
+            "{search}" User Not found
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
 
-
-
-    ) : (
-            <div className="text-2xl text-center lg:text-4xl font-bold">
-              "{search}" User Not found
-            </div>
-          )}
-
-   
-   </div>
-   </>
-  )
-}
-
-export default Alluser
+export default Alluser;
